1e0e021f5911764bad394a6d0a95f959128666dd
Ihar Mikailau <ihar.mikailau@ventionteams.com>
2025-09-26 15:21:39 +0200

    fix/auth-tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  testPathIgnorePatterns: ['/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/prisma/(.*)$': '<rootDir>/prisma/$1',
     '^@/root/path.config.js$': '<rootDir>/__mocks__/path.config.ts',
